e96945344adaac5452677c185779c36888368879
willian <wiliam-m-@hotmail.com>
2020-10-18 18:53:01 -0300

    search and insert func

--- tabelahash.cpp
+++ tabelahash.cpp
@@ -1,6 +1,59 @@
 #include "tabelahash.h"
+#include <random>
+#include <string>
+#include <cstring>
 
 tabelahash::tabelahash()
 {
+    this->MAX_SIZE = 4096;
+    tabela = new std::list<std::pair<documento*, std::string>>[MAX_SIZE];
+}
+tabelahash::tabelahash(int max_size)
+{
+    this->MAX_SIZE = max_size;
+    tabela = new std::list<std::pair<documento*, std::string>>[MAX_SIZE];
+}
+int tabelahash::funchash(std::string palavra)
+{
+    int counter = 0;
+    int i = 1;
 
+    std::srand(time(NULL));
+
+    for(char& c: palavra)
+    {
+        counter += i++ * static_cast<int>(c) + (std::rand() % MAX_SIZE);
+    }
+
+    int index = static_cast<int>(counter % MAX_SIZE);
+
+    return index;
+}
+bool tabelahash::Insere(documento *doc, std::string palavra)
+{   
+    if(!(n_entradas == MAX_SIZE))
+    {
+        int index = funchash(palavra);
+
+        tabela[index].push_back(std::make_pair(doc, palavra));
+        n_entradas += 1;
+        return true;
+    }
+    return false;
+}
+std::vector<std::pair<documento*, std::string>> tabelahash::busca(std::string palavra)
+{
+    int index = funchash(palavra);
+
+    std::vector<std::pair<documento*, std::string>> tmp;
+
+    for(auto it=tabela[index].begin(); it != tabela[index].end(); ++it)
+    {
+        if(palavra.compare(it->second) == 0)
+        {
+            tmp.push_back(std::make_pair(it->first, it->second));
+        }
+    }
+
+    return tmp;
 }
--- tabelahash.h
+++ tabelahash.h
@@ -1,11 +1,26 @@
 #ifndef TABELAHASH_H
 #define TABELAHASH_H
+#include <documento.h>
+#include <list>
 
 
 class tabelahash
 {
 public:
     tabelahash();
+    tabelahash(int max_size);
+
+    bool TabelaCheia();
+    bool Insere(documento *doc, std::string palavra);
+    std::vector<std::pair<documento*, std::string>> busca(std::string palavra);
+    int funchash(std::string palavra);
+
+private:
+    std::list<std::pair<documento*, std::string>> *tabela;
+    unsigned int n_entradas = 0;
+    int MAX_SIZE;
+
+
 };
 
 #endif // TABELAHASH_H
